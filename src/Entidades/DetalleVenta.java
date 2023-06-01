3d81e4a324629b4db03a6a16d778958fb8b7fabc
Vega Alberto Daniel <danielveg2013@gmail.com>
2023-06-01 16:35:59 -0300

    Actualizacion Entidades
    DetalleVenta y Producto

--- src/Entidades/DetalleVenta.java
+++ src/Entidades/DetalleVenta.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Entidades;
 
 /**
@@ -9,5 +5,73 @@ package Entidades;
  * @author Usuario
  */
 public class DetalleVenta {
+    private int idDetalleVent;
+    private int cantidad;
+    private double precioVenta;
+    private Venta venta;
+    private Producto producto;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(int cantidad, double precioVenta, Venta venta, Producto producto) {
+        this.cantidad = cantidad;
+        this.precioVenta = precioVenta;
+        this.venta = venta;
+        this.producto = producto;
+    }
+
+    public DetalleVenta(int idDetalleVent, int cantidad, double precioVenta, Venta venta, Producto producto) {
+        this.idDetalleVent = idDetalleVent;
+        this.cantidad = cantidad;
+        this.precioVenta = precioVenta;
+        this.venta = venta;
+        this.producto = producto;
+    }
+
+    public int getIdDetalleVent() {
+        return idDetalleVent;
+    }
+
+    public void setIdDetalleVent(int idDetalleVent) {
+        this.idDetalleVent = idDetalleVent;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleVenta{" + "idDetalleVent=" + idDetalleVent + ", cantidad=" + cantidad + ", precioVenta=" + precioVenta + ", venta=" + venta + ", producto=" + producto + '}';
+    }
     
 }
--- src/Entidades/Producto.java
+++ src/Entidades/Producto.java
@@ -6,5 +6,72 @@ package Entidades;
  */
 public class Producto {
     private int id;
+    private String nombre;////agregar DB
+    private double precioActual;
+    private int stock;
+    private boolean estado;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precioActual, int stock, boolean estado) {
+        this.nombre = nombre;
+        this.precioActual = precioActual;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public Producto(int id, String nombre, double precioActual, int stock, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precioActual = precioActual;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioActual() {
+        return precioActual;
+    }
+
+    public void setPrecioActual(double precioActual) {
+        this.precioActual = precioActual;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", nombre=" + nombre + ", precioActual=" + precioActual + ", stock=" + stock + ", estado=" + estado + '}';
+    }
     
 }
