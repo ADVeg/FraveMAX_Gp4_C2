5edb671ea694d24612f37a08b9273d6680d9603e
Vega Alberto Daniel <danielveg2013@gmail.com>
2023-06-01 12:17:13 -0300

    Creación del Proyecto

---
+++ src/fravemax_gp4_c2/FraveMAX_Gp4_C2.java
@@ -0,0 +1,15 @@
+package fravemax_gp4_c2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FraveMAX_Gp4_C2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
