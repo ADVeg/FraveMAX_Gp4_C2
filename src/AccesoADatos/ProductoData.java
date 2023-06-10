f24a25a5c5188da9f5d660ed73cc877eff6305e5
Vega Alberto Daniel <danielveg2013@gmail.com>
2023-06-10 11:17:08 -0300

    Update ProductoData

--- src/AccesoADatos/ProductoData.java
+++ src/AccesoADatos/ProductoData.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -67,5 +69,129 @@ public class ProductoData {
             Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
         }
         return producto;
+    }
+    
+    public Producto buscarID(int id){
+        Producto producto=null;
+        String sql="SELECT * FROM producto WHERE idProducto=? AND estado=true";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                producto=new Producto();
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setNombre(rs.getString("nombre"));
+                producto.setCategoria(rs.getString("categoria"));
+                producto.setDescripcion(rs.getString("descripcion"));
+                producto.setPrecioActual(rs.getInt("precioActual"));
+                producto.setStock(rs.getInt("stock"));
+                producto.setEstado(rs.getBoolean("estado"));
+            }else{
+                System.out.println("Producto no encontrado");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return producto;
+    }
+    
+    public List<Producto> buscarCategoria(String categoria){
+        List<Producto> productos=new ArrayList<>();
+        String sql="SELECT * FROM producto WHERE categoria=? AND estado=true";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setString(1, categoria);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                productos.add(new Producto(rs.getInt("idProducto"),rs.getString("nombre"),rs.getString("categoria"),rs.getString("descripcion"),rs.getDouble("precioActual"),rs.getInt("stock"),rs.getBoolean("estado")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return productos;
+    }
+    
+    public void bajaProducto(int id){
+        String sql="UPDATE producto SET estado=false WHERE idProducto=?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id);
+            if(ps.executeUpdate()==1){
+                System.out.println("Producto dado de baja");
+            }else{
+                System.out.println("Producto no encontrado");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void altaProducto(int id){
+        String sql="UPDATE producto SET estado=true WHERE idProducto=?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, id);
+            if(ps.executeUpdate()==1){
+                System.out.println("Producto dado de alta");
+            }else{
+                System.out.println("Producto no encontrado");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void modificarProducto(Producto prod){
+        String sql="UPDATE producto SET nombre=?, categoria=?, descripcion=?, precioActual=?, stock=?, estado=? WHERE idProducto=?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, prod.getIdProducto());
+            ps.setString(2, prod.getNombre());
+            ps.setString(3, prod.getCategoria());
+            ps.setString(4, prod.getDescripcion());
+            ps.setDouble(5, prod.getPrecioActual());
+            ps.setInt(6, prod.getStock());
+            ps.setBoolean(7, prod.isEstado());
+            if(ps.executeUpdate()==1){
+                System.out.println("Producto modificado");
+            }else{
+                System.out.println("Producto no encontrado");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public List<Producto> productosEnAlta(){
+        List<Producto> productos=new ArrayList<>();
+        String sql="SELECT * FROM producto WHERE estado=true";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                productos.add(new Producto(rs.getInt("idProducto"),rs.getString("nombre"),rs.getString("categoria"),rs.getString("descripcion"),rs.getDouble("precioActual"),rs.getInt("stock"),rs.getBoolean("estado")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return productos;
+    }
+    
+    public List<Producto> productosEnBaja(){
+        List<Producto> productos=new ArrayList<>();
+        String sql="SELECT * FROM producto WHERE estado=false";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                productos.add(new Producto(rs.getInt("idProducto"),rs.getString("nombre"),rs.getString("categoria"),rs.getString("descripcion"),rs.getDouble("precioActual"),rs.getInt("stock"),rs.getBoolean("estado")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return productos;
     }
 }
